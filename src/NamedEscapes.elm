72f591c8e4061edb3c186c14b98a6135ee7390dc
Jason Mahr <jasonmahr2017@gmail.com>
2016-05-26 02:41:04 +0800

    elm-format 0.3
    4 spaces

--- src/NamedEscapes.elm
+++ src/NamedEscapes.elm
@@ -1,4 +1,4 @@
-module NamedEscapes (codeToStr, getNamedEscape, subm, regexAll) where
+module NamedEscapes exposing (codeToStr, getNamedEscape, subm, regexAll)
 
 import Char exposing (fromCode, toCode)
 import Dict exposing (Dict, fromList, get)
